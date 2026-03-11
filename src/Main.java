5eb1984dc9f90bf9cfb7a8289e967d50d2e36b82
siddukummetha9-droid <siddukummetha9@gmail.com>
2026-03-11 10:54:47 +0530

    uc3:stringreverse

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class UseCase2PalindromeCheck {
+public class UseCase3PalindromeCheck {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String str = sc.nextLine();
+        String input = sc.nextLine();
 
-        String rev = "";
+        String reversed = "";
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev = rev + str.charAt(i);
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        if (str.equals(rev)) {
+        if (input.equals(reversed)) {
             System.out.println("Palindrome: True");
         } else {
             System.out.println("Palindrome: False");
